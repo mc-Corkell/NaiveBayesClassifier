adb7e9304092424c97946be026d549ff70c31dd2
mc_corkell <katiemccorkell@gmail.com>
2014-02-25 16:30:01 -0800

    removing a few files

--- src/Classifier.java
+++
@@ -1,8 +0,0 @@
-
-public interface Classifier {
-	
-	// Returns 1 if example is classified as part of + class 
-	public double vote(String example); 
-	
-	public Classifier(Instances instances); 
-}
--- src/ID3.java
+++
@@ -1,9 +0,0 @@
-
-public class ID3 implements Classifier {
-
-	@Override
-	public int vote(String example) {
-		return 0;
-	}
-
-}
